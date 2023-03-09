9d00e5f7f853830511f779f5b70fb340406ec1cc
angelherce <angel.herce@imatia.com>
2023-03-09 12:22:36 +0100

    Section 06: V054 - UNIQUE constraint - exercise - Se ha actualizado el script de la base de datos "sales" para añadir las instrucciones para añadir una nueva columna a la tabla "customers" con la instrucción "ALTER". - Se ha actualizado el script de la base de datos "sales" para añadir las instrucciones para añadir unregistro en la tabla "customers".

--- work/databases/sales.database.sql
+++ work/databases/sales.database.sql
@@ -47,4 +47,8 @@ ALTER TABLE sales
         ON DELETE CASCADE;
 
 ALTER TABLE customers
+    ADD COLUMN gender ENUM('M','F') AFTER last_name,
     DROP INDEX email_address;
+
+INSERT INTO customers ( first_name, last_name, gender, email_address, number_of_complaints )
+    VALUES ( 'John', 'Mackinley', 'M', 'john.mckinley@365careers.com', 0 );

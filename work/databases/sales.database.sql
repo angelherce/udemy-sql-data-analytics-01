e3fef40fc89c432beff7915668fb33d60b823c0e
angelherce <angel.herce@imatia.com>
2023-03-09 11:23:59 +0100

    Section 05: V038 - Creating a Table - Se ha guardado el PDF del video - Se ha actualizado el script de la base de datos "sales" para añadir la instrucción que crea la tabla "sales".

--- work/databases/sales.database.sql
+++ work/databases/sales.database.sql
@@ -2,3 +2,10 @@ CREATE DATABASE IF NOT EXISTS sales CHARACTER SET utf8mb4 COLLATE utf8mb4_genera
 -- CREATE SCHEMA IF NOT EXISTS sales CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;;
 
 USE sales;
+
+CREATE TABLE sales (
+	purchase_number INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    date_of_purchase DATE NOT NULL,
+    customer_id INT,
+    item_code VARCHAR( 10 ) NOT NULL
+);

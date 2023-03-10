636f2559b33fcd535696ca58482190e57af38eea
angelherce <angel.herce@imatia.com>
2023-03-10 19:08:38 +0100

    Section 12: V140 - The DELETE Statement - Part II - Se han replicado los ejemplos realizados en el video.

---
+++ work/section 09/exercises/10.140.delete.sql
@@ -0,0 +1,28 @@
+
+USE employees;
+
+COMMIT;
+
+SELECT *
+    FROM departments_dup
+    ORDER BY dept_no;
+
+INSERT INTO departments_dup ( dept_no, dept_name ) VALUES
+    ( 'd001', 'Department 01' ),
+    ( 'd002', 'Department 02' ),
+    ( 'd003', 'Department 03' ),
+    ( 'd004', 'Department 04' ),
+    ( 'd005', 'Department 05' ),
+    ( 'd006', 'Department 06' ),
+    ( 'd007', 'Department 07' ),
+    ( 'd008', 'Department 08' ),
+    ( 'd009', 'Department 09' ),
+    ( 'd010', 'Department 10' );
+
+DELETE FROM departments_dup;
+
+SELECT *
+    FROM departments_dup
+    ORDER BY dept_no;
+
+ROLLBACK;

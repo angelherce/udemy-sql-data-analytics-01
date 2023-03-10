5425d0bd906b50c242393f9512a6f5fd51800e54
angelherce <angel.herce@imatia.com>
2023-03-10 09:57:46 +0100

    Section 09: V086 - LIKE - NOT LIKE - Se han replicado los ejemplos realizados en el video.

---
+++ work/section 09/exercises/09.086.like.not-like.sql
@@ -0,0 +1,29 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name LIKE ( 'Mar%' );
+
+SELECT *
+FROM employees
+WHERE first_name LIKE ( '%ar' );
+
+SELECT *
+FROM employees
+WHERE first_name LIKE ( '%ar%' );
+
+SELECT *
+FROM employees
+WHERE first_name LIKE ( 'Mar_' );
+
+SELECT *
+FROM employees
+WHERE first_name NOT LIKE ( '%Mar%' );
+
+SELECT *
+FROM employees
+WHERE first_name NOT LIKE ( '%mar%' );
+
+SELECT *
+FROM employees
+WHERE first_name NOT LIKE ( '%MAR%' );

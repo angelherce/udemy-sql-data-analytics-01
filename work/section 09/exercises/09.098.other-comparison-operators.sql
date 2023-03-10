279aafadaceceb44f1994a1a2e011df899c69023
angelherce <angel.herce@imatia.com>
2023-03-10 10:34:42 +0100

    Section 09: V098 - Other comparision operators - Se han replicado los ejemplos realizados en el video.

---
+++ work/section 09/exercises/09.098.other-comparison-operators.sql
@@ -0,0 +1,29 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name = 'Mark';
+
+SELECT *
+FROM employees
+WHERE first_name <> 'Mark';
+
+SELECT *
+FROM employees
+WHERE first_name != 'Mark';
+
+SELECT *
+FROM employees
+WHERE hire_date > '2000-01-01';
+
+SELECT *
+FROM employees
+WHERE hire_date >= '2000-01-01';
+
+SELECT *
+FROM employees
+WHERE hire_date < '1985-02-01';
+
+SELECT *
+FROM employees
+WHERE hire_date <= '1985-02-01';

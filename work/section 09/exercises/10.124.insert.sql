7c3d77cd6197a1a1452b4c40fde3ed084a556897
angelherce <angel.herce@imatia.com>
2023-03-10 13:06:50 +0100

    Section 10: V124 - The INSERT statement - Part I - Se han replicado los ejemplos realizados en el video.

---
+++ work/section 09/exercises/10.124.insert.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+SELECT *
+FROM employees
+ORDER BY emp_no DESC
+LIMIT 10;
+
+INSERT INTO employees ( emp_no, birth_date, first_name, last_name, gender, hire_date )
+    VALUES ( 999901, '1986-04-01', 'John', 'Smith', 'M', '2011-01-01' );
+
+SELECT *
+FROM employees
+ORDER BY emp_no DESC
+LIMIT 10;

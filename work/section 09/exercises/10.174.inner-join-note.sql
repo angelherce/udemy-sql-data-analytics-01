9178ddbdfff3b5832b9c5697572979d390fb770e
angelherce <angel.herce@imatia.com>
2023-03-12 12:17:00 +0100

    Section 14: V174 - A Note on Using Joins - Se ha corregido un error

--- work/section 09/exercises/10.174.inner-join-note.sql
+++ work/section 09/exercises/10.174.inner-join-note.sql
@@ -1,12 +1,12 @@
 USE employees;
 
-SELECT m.dept_no, m.emp_no, m.from_date, m.to_datem d.dept_name
+SELECT m.dept_no, m.emp_no, m.from_date, m.to_date, d.dept_name
     FROM dept_manager_dup m
     INNER JOIN departments_dup d
         ON d.dept_no = m.dept_no
     ORDER BY m.dept_no;
 
- SELECT m.dept_no, m.emp_no, m.from_date, m.to_datem d.dept_name
+ SELECT m.dept_no, m.emp_no, m.from_date, m.to_date, d.dept_name
      FROM dept_manager_dup m
      JOIN departments_dup d
          ON d.dept_no = m.dept_no
